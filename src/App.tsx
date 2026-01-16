46f149dcf820e25535d7f45a470bce4e05ec3caf
mrauthentik <umokeuchenna2020@gmail.com>
2026-01-16 12:48:59 +0100

    Add Skills, Projects, and Project Detail sections to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
+import SkillsSection from './components/SkillsSection'
+import ProjectsSection from './components/ProjectsSection'
+import ProjectDetailSection from './components/ProjectDetailSection'
 
 function App() {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ProjectDetailSection />
     </div>
   )
 }
